bafedbe3fc642ea48ae28c2ebe891221f6bba732
monikassingh <monika.ssingh@gmail.com>
2020-03-06 20:21:01 -0500

    Updated

---
+++ CareerCloud.WebAPI/Controllers/ApplicantEducationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc; 
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class ApplicantEducationController : ControllerBase
+    {
+        private readonly ApplicantEducationLogic _logic;
+
+        public ApplicantEducationController()
+        {
+            var repo = new EFGenericRepository<ApplicantEducationPoco>();
+            _logic = new ApplicantEducationLogic(repo); 
+        }
+
+        [HttpGet]
+        [Route("education/{Id}")]
+        public ActionResult GetApplicantEducation(Guid Id)
+        {
+            ApplicantEducationPoco poco = _logic.Get(Id);
+            if(poco==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(poco);
+            }
+            
+        }
+    }
+}
---
+++ CareerCloud.WebAPI/Controllers/ApplicantJobApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class ApplicantJobApplicationController : ControllerBase
+    {
+        private readonly ApplicantJobApplicationLogic _logic;
+
+        public ApplicantJobApplicationController()
+        {
+            var repo = new EFGenericRepository<ApplicantJobApplicationPoco>();
+            _logic = new ApplicantJobApplicationLogic(repo);
+        }
+    }
+}
